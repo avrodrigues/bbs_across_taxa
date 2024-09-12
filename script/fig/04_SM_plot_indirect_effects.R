f9f4a6cdea7ea40d37f6a44a4489de3fced94e15
Rissanen <tuulriss@ad.helsinki.fi>
2024-09-12 10:03:01 +0300

    indirect effect plots and updated script

--- script/fig/04_SM_plot_indirect_effects.R
+++ script/fig/04_SM_plot_indirect_effects.R
@@ -33,13 +33,67 @@ buts_fdd <- group_variables_for_plotting(buts_fdd)
 buts_gdd_plot <- plot_indirect_effects(buts_gdd, var_groups_col, "Butterflies_gdd")
 buts_fdd_plot <- plot_indirect_effects(buts_fdd, var_groups_col, "Butterflies_fdd")
 
+# Moths ----
+moths_gdd <- readRDS("./output/SEM_results/indir_effects/moths_indir_gdd.rds")
+moths_fdd <- readRDS("./output/SEM_results/indir_effects/moths_indir_fdd.rds")
 
-ggsave(
-  "output/figs/SEM_plot/SM/01_birds.pdf",
-  SM_birds_plot,
-  width = 10,
-  height = 12.5
-)
+moths_gdd <- group_variables_for_plotting(moths_gdd)
+moths_fdd <- group_variables_for_plotting(moths_fdd)
+
+moths_gdd_plot <- plot_indirect_effects(moths_gdd, var_groups_col, "Moths_gdd")
+moths_fdd_plot <- plot_indirect_effects(moths_fdd, var_groups_col, "Moths_fdd")
+
+# Phytoplankton ----
+phyto_gdd <- readRDS("./output/SEM_results/indir_effects/phyto_indir_gdd.rds")
+phyto_fdd <- readRDS("./output/SEM_results/indir_effects/phyto_indir_fdd.rds")
+
+phyto_gdd <- group_variables_for_plotting(phyto_gdd)
+phyto_fdd <- group_variables_for_plotting(phyto_fdd)
+
+phyto_gdd_plot <- plot_indirect_effects(phyto_gdd, var_groups_col, "Phytoplankton_gdd")
+phyto_fdd_plot <- plot_indirect_effects(phyto_fdd, var_groups_col, "Phytoplankton_fdd")
+
+# Small mammals ----
+rods_gdd <- readRDS("./output/SEM_results/indir_effects/rodents_indir_gdd.rds")
+rods_fdd <- readRDS("./output/SEM_results/indir_effects/rodents_indir_fdd.rds")
+
+rods_gdd <- group_variables_for_plotting(rods_gdd)
+rods_fdd <- group_variables_for_plotting(rods_fdd)
+
+rods_gdd_plot <- plot_indirect_effects(rods_gdd, var_groups_col, "Rodents_gdd")
+rods_fdd_plot <- plot_indirect_effects(rods_fdd, var_groups_col, "Rodents_fdd")
+
+# Large mammals ----
+wgame_gdd <- readRDS("./output/SEM_results/indir_effects/wgame_indir_gdd.rds")
+wgame_fdd <- readRDS("./output/SEM_results/indir_effects/wgame_indir_fdd.rds")
+
+wgame_gdd <- group_variables_for_plotting(wgame_gdd)
+wgame_fdd <- group_variables_for_plotting(wgame_fdd)
+
+wgame_gdd_plot <- plot_indirect_effects(wgame_gdd, var_groups_col, "Wgame_gdd")
+wgame_fdd_plot <- plot_indirect_effects(wgame_fdd, var_groups_col, "Wgame_fdd")
+
+# Arrange and save plots ----
+gdd_plots <- grid.arrange(birds_gdd_plot, buts_gdd_plot,
+             moths_gdd_plot, phyto_gdd_plot,
+             rods_gdd_plot,  wgame_gdd_plot,
+             nrow=3, ncol=2)
+
+ggsave("output/figs/indir_effects/indir_eff_gdd_SEMs.pdf",
+       gdd_plots,
+       width = 10,
+       height = 12.5)
+
+fdd_plots <- grid.arrange(birds_fdd_plot, buts_fdd_plot,
+             moths_fdd_plot, phyto_fdd_plot,
+             rods_fdd_plot, wgame_fdd_plot,
+             nrow=3, ncol=2)
+
+ggsave("output/figs/indir_effects/indir_eff_fdd_SEMs.pdf",
+       fdd_plots,
+       width = 10,
+       height = 12.5)
+
 
 
 
